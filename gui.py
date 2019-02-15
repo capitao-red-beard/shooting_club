2b224e5b11d462f561fba5cd921310fd737ec04c
adriaan.van.der.valk@samskip.com <!Qazxswedcvfr>
2019-02-15 13:30:21 +0100

    added the logo

--- gui.py
+++ gui.py
@@ -1,8 +1,42 @@
 import tkinter as tk
 from tkinter import ttk
+import database
 
 
-LARGE_FONT = ("Verdana", 10)
+LARGE_FONT = ("Verdana", 12)
+NORMAL_FONT = ("Verdana", 10)
+SMALL_FONT = ("Verdana", 8)
+
+
+def popup_new_user():
+    popup = tk.Tk()
+    popup.wm_title("Create a New User")
+
+    label_first_name = ttk.Label(popup, text="First Name:").grid(row=0, column=0)
+    entry_first_name = ttk.Entry(popup, width=15).grid(row=0, column=1)
+
+    label_last_name = ttk.Label(popup, text="Last Name:").grid(row=1, column=0)
+    entry_last_name = ttk.Entry(popup, width=15).grid(row=1, column=1)
+
+    label_user_type = ttk.Label(popup, text="User Type:").grid(row=2, column=0)
+    default_value = tk.StringVar(popup)
+    default_value.set("select")
+    option_menu_user_type = tk.OptionMenu(popup, default_value, "Regular", "Super", "Admin").grid(row=2, column=1)
+
+    label_licence_number = ttk.Label(popup, text="Licence Number:").grid(row=3, column=0)
+    entry_licence_number = ttk.Entry(popup, width=15).grid(row=3, column=1)
+
+    label_email = ttk.Label(popup, text="Email:").grid(row=4, column=0)
+    entry_email = ttk.Entry(popup, width=15).grid(row=4, column=1)
+
+    label_password = ttk.Label(popup, text="Password:").grid(row=5, column=0)
+    entry_password = ttk.Entry(popup, show="*", width=15).grid(row=5, column=1)
+
+    button_submit = ttk.Button(popup, text="Submit").grid(row=6, column=0)
+    button_cancel = ttk.Button(popup, text="Cancel", command=popup.destroy).grid(row=6, column=1)
+
+    popup.pack()
+    popup.mainloop()
 
 
 class ShootingClub(tk.Tk):
@@ -12,11 +46,21 @@ class ShootingClub(tk.Tk):
         tk.Tk.__init__(self, *args, **kwargs)
 
         tk.Tk.iconbitmap(self, default="logo_16_16.ico")
+        tk.Tk.wm_title(self, "Shooting Club")
 
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
+
+        menu_bar = tk.Menu(container)
+        file_menu = tk.Menu(menu_bar, tearoff=0)
+        file_menu.add_command(label="New User", command=lambda: popup_new_user())
+        file_menu.add_separator()
+        file_menu.add_command(label="Exit", command=quit)
+        menu_bar.add_cascade(label="File", menu=file_menu)
+
+        tk.Tk.config(self, menu=menu_bar)
 
         self.frames = {}
 
@@ -80,4 +124,5 @@ class PageTwo(tk.Frame):
 
 
 app = ShootingClub()
+app.geometry("1280x720")
 app.mainloop()
