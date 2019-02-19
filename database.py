a7ad7edc420d50874a74359738492458e1c24bf0
adriaan.van.der.valk@samskip.com <!Qazxswedcvfr>
2019-02-19 19:05:45 +0100

    edited the GUI and database to better fit user needs

--- database.py
+++ database.py
@@ -2,29 +2,92 @@ import sqlite3
 from sqlite3 import Error
 
 
-statement1 = '''CREATE TABLE IF NOT EXISTS user (
-                            type NUMBER NOT NULL,
+statement1 = '''CREATE TABLE IF NOT EXISTS member (
+                            user_type INTEGER NOT NULL,
                             first_name TEXT NOT NULL,
                             infix TEXT NOT NULL,
                             surname TEXT NOT NULL,
-                            date_of_birth DATE NOT NULL,
+                            date_of_birth TEXT NOT NULL,
                             address TEXT NOT NULL,
                             city TEXT NOT NULL,
                             post_code TEXT NOT NULL,
-                            telephone_number NUMBER NOT NULL,
+                            telephone_number INTEGER NOT NULL,
                             email_address TEXT UNIQUE NOT NULL,
                             password TEXT NOT NULL,
-                            knsa_licence_number NUMBER PRIMARY KEY,
-                            date_of_membership DATE NOT NULL);'''
+                            knsa_licence_number INTEGER PRIMARY KEY,
+                            date_of_membership TEXT NOT NULL);'''
 
-statement2 = '''INSERT OR IGNORE INTO user
-                VALUES ();
-                '''
+statement2 = '''CREATE TABLE IF NOT EXISTS weapon (
+                            weapon_type TEXT NOT NULL,
+                            owner INTEGER NOT NULL,
+                            PRIMARY KEY (weapon_type, owner),
+                            FOREIGN KEY (owner) REFERENCES member (knsa_licence_number)
+                            ON DELETE CASCADE ON UPDATE CASCADE);'''
 
-statement3 = 'SELECT * FROM user;'
+statement3 = '''CREATE TABLE IF NOT EXISTS ammunition (
+                            ammunition_type TEXT PRIMARY KEY,
+                            price REAL NOT NULL,
+                            stock INTEGER);'''
 
-statement4 = '''CREATE TABLE IF NOT EXISTS weapon (
-                            weapon TEXT PRIMARY KEY);'''
+statement4 = '''CREATE TABLE IF NOT EXISTS score_card (
+                            score_card_type TEXT PRIMARY KEY,
+                            price REAL NOT NULL,
+                            stock INTEGER);'''
+
+statement5 = '''CREATE TABLE IF NOT EXISTS score (
+                            score_id INTEGER PRIMARY KEY AUTOINCREMENT,                            
+                            shot_one INTEGER NOT NULL,
+                            shot_two INTEGER NOT NULL,
+                            shot_three INTEGER NOT NULL,
+                            shot_four INTEGER NOT NULL,
+                            shot_five INTEGER NOT NULL,
+                            date_score TEXT NOT NULL,
+                            ammunition_type TEXT NOT NULL,                            
+                            shooter INTEGER NOT NULL,                            
+                            weapon_type TEXT NOT NULL,                           
+                            card_type TEXT NOT NULL,
+                            FOREIGN KEY (card_type) REFERENCES score_card (card_type)
+                            ON DELETE NO ACTION ON UPDATE CASCADE,
+                            FOREIGN KEY (ammunition_type) REFERENCES ammunition (ammunition_type)
+                            ON DELETE NO ACTION ON UPDATE CASCADE,
+                            FOREIGN KEY (shooter) REFERENCES member (knsa_licence_number)
+                            ON DELETE NO ACTION ON UPDATE CASCADE,
+                            FOREIGN KEY (weapon_type) REFERENCES weapon (weapon_type)
+                            ON DELETE NO ACTION ON UPDATE CASCADE);'''
+
+statement6 = '''CREATE TABLE IF NOT EXISTS sale_score_card (
+                            sale_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            score_card_type TEXT NOT NULL,
+                            date_sold TEXT NOT NULL,
+                            quantity INTEGER NOT NULL,
+                            seller INTEGER NOT NULL,
+                            buyer INTEGER NOT NULL,
+                            price REAL NOT NULL,                            
+                            FOREIGN KEY (card_type) REFERENCES score_card (card_type)
+                            ON DELETE NO ACTION ON UPDATE NO ACTION,
+                            FOREIGN KEY (seller) REFERENCES member (knsa_licence_number)
+                            ON DELETE NO ACTION ON UPDATE CASCADE,
+                            FOREIGN KEY (buyer) REFERENCES member (knsa_licence_number)
+                            ON DELETE NO ACTION ON UPDATE CASCADE,
+                            FOREIGN KEY (price) REFERENCES score_card (price)
+                            ON DELETE NO ACTION ON UPDATE NO ACTION);'''
+
+statement7 = '''CREATE TABLE IF NOT EXISTS sale_ammunition (
+                            sale_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            ammunition_type TEXT NOT NULL,
+                            date_sold NOT NULL,
+                            quantity INTEGER NOT NULL,
+                            seller INTEGER NOT NULL,
+                            buyer INTEGER NOT NULL,
+                            price REAL NOT NULL,
+                            FOREIGN KEY (score_cardammunition_type) REFERENCES ammunition (ammunition_type)
+                            ON DELETE NO ACTION ON UPDATE NO ACTION,
+                            FOREIGN KEY (seller) REFERENCES member (knsa_licence_number)
+                            ON DELETE NO ACTION ON UPDATE CASCADE,
+                            FOREIGN KEY (buyer) REFERENCES member (knsa_licence_number)
+                            ON DELETE NO ACTION ON UPDATE CASCADE,
+                            FOREIGN KEY (price) REFERENCES score_card (price)
+                            ON DELETE NO ACTION ON UPDATE NO ACTION);'''
 
 
 def create_connection():
@@ -58,4 +121,10 @@ def execute_sql(sql):
         print(e)
 
 
-# execute_sql(statement4)
+'''execute_sql(statement1)
+execute_sql(statement2)
+execute_sql(statement3)
+execute_sql(statement4)
+execute_sql(statement5)
+execute_sql(statement6)
+execute_sql(statement7)'''
--- gui.py
+++ gui.py
@@ -11,72 +11,74 @@ SMALL_FONT = ("Verdana", 8)
 
 def popup_new_user():
     popup = tk.Tk()
-    popup.wm_title("Create a New User")
+    popup.wm_title("Nieuw Lid")
 
-    label_user_type = ttk.Label(popup, text="Gebruikerstype:").grid(row=0, column=0, padx=5, pady=5, sticky="W")
+    label_user_type = ttk.Label(popup, text="Gebruikerstype:").grid(row=0, column=0, padx=5, pady=2, sticky="W")
     options = ("Standaard", "Beheerder")
     value_user_type = tk.StringVar(popup)
     value_user_type.set("Select")
     option_menu_user_type = ttk.OptionMenu(popup, value_user_type, options[0], *options)\
-        .grid(row=0, column=1, padx=5, pady=5)
+        .grid(row=0, column=1, padx=5, pady=2)
 
-    label_first_name = ttk.Label(popup, text="Voornaam:").grid(row=1, column=0, padx=5, pady=5, sticky="W")
+    label_first_name = ttk.Label(popup, text="Voornaam:").grid(row=1, column=0, padx=5, pady=2, sticky="W")
     value_first_name = tk.StringVar()
-    entry_first_name = ttk.Entry(popup, textvariable=value_first_name, width=20).grid(row=1, column=1, padx=5, pady=5)
+    entry_first_name = ttk.Entry(popup, textvariable=value_first_name, width=20).grid(row=1, column=1, padx=5, pady=2)
 
-    label_infix = ttk.Label(popup, text="Tussenvoegsel:").grid(row=2, column=0, padx=5, pady=5, sticky="W")
+    label_infix = ttk.Label(popup, text="Tussenvoegsel:").grid(row=2, column=0, padx=5, pady=2, sticky="W")
     value_infix = tk.StringVar()
-    entry_infix = ttk.Entry(popup, textvariable=value_infix, width=20).grid(row=2, column=1, padx=5, pady=5)
+    entry_infix = ttk.Entry(popup, textvariable=value_infix, width=20).grid(row=2, column=1, padx=5, pady=2)
 
-    label_surname = ttk.Label(popup, text="Familienaam:").grid(row=3, column=0, padx=5, pady=5, sticky="W")
+    label_surname = ttk.Label(popup, text="Familienaam:").grid(row=3, column=0, padx=5, pady=2, sticky="W")
     value_surname = tk.StringVar()
-    entry_surname = ttk.Entry(popup, textvariable=value_surname, width=20).grid(row=3, column=1, padx=5, pady=5)
+    entry_surname = ttk.Entry(popup, textvariable=value_surname, width=20).grid(row=3, column=1, padx=5, pady=2)
 
-    label_date_of_birth = ttk.Label(popup, text="Geboorte Datum:").grid(row=4, column=0, padx=5, pady=5, sticky="W")
+    label_date_of_birth = ttk.Label(popup, text="Geboorte Datum (YYYY-MM-DD):")\
+        .grid(row=4, column=0, padx=5, pady=2, sticky="W")
     value_date_of_birth = tk.StringVar()
     entry_date_of_birth = ttk.Entry(popup, textvariable=value_date_of_birth, width=20)\
-        .grid(row=4, column=1, padx=5, pady=5)
+        .grid(row=4, column=1, padx=5, pady=2)
 
-    label_address = ttk.Label(popup, text="Adres:").grid(row=5, column=0, padx=5, pady=5, sticky="W")
+    label_address = ttk.Label(popup, text="Adres:").grid(row=5, column=0, padx=5, pady=2, sticky="W")
     value_address = tk.StringVar()
-    entry_address = ttk.Entry(popup, textvariable=value_address, width=20).grid(row=5, column=1, padx=5, pady=5)
+    entry_address = ttk.Entry(popup, textvariable=value_address, width=20).grid(row=5, column=1, padx=5, pady=2)
 
     label_city = ttk.Label(popup, text="Woonplaats:").grid(row=6, column=0, padx=5, pady=5, sticky="W")
     value_city = tk.StringVar()
-    entry_city = ttk.Entry(popup, textvariable=value_city, width=20).grid(row=6, column=1, padx=5, pady=5)
+    entry_city = ttk.Entry(popup, textvariable=value_city, width=20).grid(row=6, column=1, padx=5, pady=2)
 
-    label_post_code = ttk.Label(popup, text="Postcode:").grid(row=7, column=0, padx=5, pady=5, sticky="W")
+    label_post_code = ttk.Label(popup, text="Postcode:").grid(row=7, column=0, padx=5, pady=2, sticky="W")
     value_post_code = tk.StringVar()
-    entry_post_code = ttk.Entry(popup, textvariable=value_post_code, width=20).grid(row=7, column=1, padx=5, pady=5)
+    entry_post_code = ttk.Entry(popup, textvariable=value_post_code, width=20).grid(row=7, column=1, padx=5, pady=2)
 
-    label_telephone_number = ttk.Label(popup, text="Telefoonnummer:").grid(row=8, column=0, padx=5, pady=5, sticky="W")
+    label_telephone_number = ttk.Label(popup, text="Telefoonnummer:").grid(row=8, column=0, padx=5, pady=2, sticky="W")
     value_telephone_number = tk.StringVar()
     entry_telephone_number = ttk.Entry(popup, textvariable=value_telephone_number, width=20)\
-        .grid(row=8, column=1, padx=5, pady=5)
+        .grid(row=8, column=1, padx=5, pady=2)
 
-    label_email_address = ttk.Label(popup, text="Email Adres:").grid(row=9, column=0, padx=5, pady=5, sticky="W")
+    label_email_address = ttk.Label(popup, text="Email Adres:").grid(row=9, column=0, padx=5, pady=2, sticky="W")
     value_email_address = tk.StringVar()
     entry_email_address = ttk.Entry(popup, textvariable=value_email_address, width=20)\
-        .grid(row=9, column=1, padx=5, pady=5)
+        .grid(row=9, column=1, padx=5, pady=2)
 
-    label_password = ttk.Label(popup, text="Wachtwoord:").grid(row=10, column=0, padx=5, pady=5, sticky="W")
+    label_password = ttk.Label(popup, text="Wachtwoord:").grid(row=10, column=0, padx=5, pady=2, sticky="W")
     value_password = tk.StringVar()
     entry_password = ttk.Entry(popup, show="*", textvariable=value_password, width=20)\
-        .grid(row=10, column=1, padx=5, pady=5)
+        .grid(row=10, column=1, padx=5, pady=2)
 
     label_knsa_licence_number = ttk.Label(popup, text="KNSA Licentienummer:")\
-        .grid(row=11, column=0, padx=5, pady=5, sticky="W")
+        .grid(row=11, column=0, padx=5, pady=2, sticky="W")
     value_knsa_licence_number = tk.StringVar()
     entry_knsa_licence_number = ttk.Entry(popup, textvariable=value_knsa_licence_number, width=20)\
-        .grid(row=11, column=1, padx=5, pady=5)
+        .grid(row=11, column=1, padx=5, pady=2)
 
-    label_date_of_membership = ttk.Label(popup, text="Datum van Lidmaatschap Ingang:")\
-        .grid(row=12, column=0, padx=5, pady=5, sticky="W")
+    label_date_of_membership = ttk.Label(popup, text="Datum van Lidmaatschap Ingang (YYYY-MM-DD):")\
+        .grid(row=12, column=0, padx=5, pady=2, sticky="W")
     value_date_of_membership = tk.StringVar()
     entry_date_of_membership = ttk.Entry(popup, textvariable=value_date_of_membership, width=20)\
-        .grid(row=12, column=1, padx=5, pady=5)
+        .grid(row=12, column=1, padx=5, pady=2)
 
-    button_submit = ttk.Button(popup, text="Submit", command=lambda: clicked()).grid(row=13, column=0, padx=10, pady=20)
+    button_submit = ttk.Button(popup, text="Submit", command=lambda: clicked())\
+        .grid(row=13, column=0, padx=120, pady=15)
 
     def clicked():
         if value_user_type.get() == 'Beheerder':
@@ -84,7 +86,21 @@ def popup_new_user():
         else:
             user_type = '1'
 
-        result = database.execute_sql('INSERT OR IGNORE INTO user VALUES ('
+        result = database.execute_sql('INSERT OR IGNORE INTO member ('
+                                      'user_type, '
+                                      'first_name, '
+                                      'infix, '
+                                      'surname, '
+                                      'date_of_birth, '
+                                      'address, '
+                                      'city, '
+                                      'post_code, '
+                                      'telephone_number, '
+                                      'email_address, '
+                                      'password, '
+                                      'knsa_licence_number, '
+                                      'date_of_membership'
+                                      ') VALUES ('
                                       + "'" + user_type + "', "
                                       + "'" + value_first_name.get() + "', "
                                       + "'" + value_infix.get() + "', "
@@ -102,37 +118,159 @@ def popup_new_user():
         if result == 'success':
             messagebox.showinfo(title="Information", message="Successfully entered a new user into the database.")
         else:
-            messagebox.showerror(title="Error", message="An error occurred: CODE=DBF-NU")
+            messagebox.showerror(title="Error", message="An error occurred: " + result)
         popup.destroy()
 
-    button_cancel = ttk.Button(popup, text="Cancel", command=popup.destroy).grid(row=13, column=1, padx=10, pady=20)
+    button_cancel = ttk.Button(popup, text="Cancel", command=popup.destroy).grid(row=13, column=1, padx=10, pady=15)
 
     popup.mainloop()
+
+
+'''def popup_edit_user():
+    popup = tk.Tk()
+    popup.wm_title("Lid Aanpassen")
+
+    label_user_type = ttk.Label(popup, text="Gebruikerstype:").grid(row=0, column=0, padx=5, pady=2, sticky="W")
+
+    members_result = database.execute_sql("SELECT knsa_licence_number, first_name, infix, surname FROM member;")
+    options = ()
+    value_user_type = tk.StringVar(popup)
+    value_user_type.set("Select")
+    option_menu_user_type = ttk.OptionMenu(popup, value_user_type, options[0], *options)\
+        .grid(row=0, column=1, padx=5, pady=2)
+
+    button_submit = ttk.Button(popup, text="Submit", command=lambda: clicked()).grid(row=2, column=0, padx=10, pady=15)
+
+    def clicked():
+        result = database.execute_sql()
+
+        if result == 'success':
+            messagebox.showinfo(title="Information", message="")
+        else:
+            messagebox.showerror(title="Error", message="An error occurred: " + result)
+        popup.destroy()
+
+    button_cancel = ttk.Button(popup, text="Cancel", command=popup.destroy).grid(row=2, column=1, padx=10, pady=15)
+
+    popup.mainloop()'''
 
 
 def popup_new_weapon():
     popup = tk.Tk()
-    popup.wm_title("Add a New Weapon")
+    popup.wm_title("Nieuw Wapen")
 
-    label_weapon = ttk.Label(popup, text="Weapon Type:").grid(row=0, column=0)
+    label_weapon = ttk.Label(popup, text="Wapen:").grid(row=0, column=0, padx=5, pady=2, sticky="W")
 
-    value = tk.StringVar()
-    entry_weapon = ttk.Entry(popup, width=15, textvariable=value).grid(row=0, column=1)
+    value_weapon = tk.StringVar()
+    entry_weapon = ttk.Entry(popup, textvariable=value_weapon, width=20).grid(row=0, column=1, padx=5, pady=2)
 
-    button_submit = ttk.Button(popup, text="Submit", command=lambda: clicked()).grid(row=2, column=0)
+    button_submit = ttk.Button(popup, text="Submit", command=lambda: clicked()).grid(row=2, column=0, padx=10, pady=15)
 
     def clicked():
-        result = database.execute_sql('INSERT OR IGNORE INTO weapon VALUES (' + "'" + value.get() + "'" + ');')
+        result = database.execute_sql('INSERT OR IGNORE INTO weapon VALUES (' + "'" + value_weapon.get() + "'" + ');')
 
         if result == 'success':
             messagebox.showinfo(title="Information", message="Successfully entered a new weapon into the database.")
         else:
-            messagebox.showerror(title="Error", message="An error occurred: CODE=DBF-NW")
+            messagebox.showerror(title="Error", message="An error occurred: " + result)
         popup.destroy()
 
-    button_cancel = ttk.Button(popup, text="Cancel", command=popup.destroy).grid(row=2, column=1)
+    button_cancel = ttk.Button(popup, text="Cancel", command=popup.destroy).grid(row=2, column=1, padx=10, pady=15)
 
     popup.mainloop()
+
+
+# def popup_edit_weapon():
+
+
+def popup_new_ammunition():
+    popup = tk.Tk()
+    popup.wm_title("Nieuwe Munitie")
+
+    label_ammunition_type = ttk.Label(popup, text="Munitie Type:").grid(row=0, column=0, padx=5, pady=2, sticky="W")
+
+    value_ammunition_type = tk.StringVar()
+    entry_ammunition_type = ttk.Entry(popup, textvariable=value_ammunition_type, width=20)\
+        .grid(row=0, column=1, padx=5, pady=2)
+
+    label_price = ttk.Label(popup, text="Munitie Prijs (EUR):").grid(row=1, column=0, padx=5, pady=2, sticky="W")
+    value_price = tk.StringVar()
+    entry_price = ttk.Entry(popup, textvariable=value_price, width=20).grid(row=1, column=1, padx=5, pady=2)
+
+    label_stock = ttk.Label(popup, text="Munitie Voorraad:").grid(row=2, column=0, padx=5, pady=2, sticky="W")
+    value_stock = tk.StringVar()
+    entry_stock = ttk.Entry(popup, textvariable=value_stock, width=20).grid(row=2, column=1, padx=5, pady=2)
+
+    button_submit = ttk.Button(popup, text="Submit", command=lambda: clicked()).grid(row=3, column=0, padx=10, pady=15)
+
+    def clicked():
+        result = database.execute_sql('INSERT OR IGNORE INTO ammunition ('
+                                      'ammunition_type, '
+                                      'price, '
+                                      'stock'
+                                      ') VALUES ('
+                                      + "'" + value_ammunition_type.get() + "', "
+                                      + "'" + value_price.get() + "', "
+                                      + "'" + value_stock.get() + "'" + ');')
+
+        if result == 'success':
+            messagebox.showinfo(title="Information",
+                                message="Successfully entered a new ammunition type into the database.")
+        else:
+            messagebox.showerror(title="Error", message="An error occurred: " + result)
+        popup.destroy()
+
+    button_cancel = ttk.Button(popup, text="Cancel", command=popup.destroy).grid(row=3, column=1, padx=10, pady=15)
+
+    popup.mainloop()
+
+
+# def popup_edit_ammunition():
+
+
+def popup_new_card():
+    popup = tk.Tk()
+    popup.wm_title("Nieuwe Kaart")
+
+    label_score_card = ttk.Label(popup, text="Kaart Type:").grid(row=0, column=0, padx=5, pady=2, sticky="W")
+
+    value_score_card = tk.StringVar()
+    entry_score_card = ttk.Entry(popup, textvariable=value_score_card, width=20) \
+        .grid(row=0, column=1, padx=5, pady=2)
+
+    label_price = ttk.Label(popup, text="Kaart Prijs (EUR):").grid(row=1, column=0, padx=5, pady=2, sticky="W")
+    value_price = tk.StringVar()
+    entry_price = ttk.Entry(popup, textvariable=value_price, width=20).grid(row=1, column=1, padx=5, pady=2)
+
+    label_stock = ttk.Label(popup, text="Kaart Voorraad:").grid(row=2, column=0, padx=5, pady=2, sticky="W")
+    value_stock = tk.StringVar()
+    entry_stock = ttk.Entry(popup, textvariable=value_stock, width=20).grid(row=2, column=1, padx=5, pady=2)
+
+    button_submit = ttk.Button(popup, text="Submit", command=lambda: clicked()).grid(row=3, column=0, padx=10, pady=15)
+
+    def clicked():
+        result = database.execute_sql('INSERT OR IGNORE INTO ammunition ('
+                                      'card_type, '
+                                      'price, '
+                                      'stock'
+                                      ') VALUES ('
+                                      + "'" + value_ammunition.get() + "', "
+                                      + "'" + value_price.get() + "', "
+                                      + "'" + value_stock.get() + "'" + ');')
+
+        if result == 'success':
+            messagebox.showinfo(title="Information",
+                                message="Successfully entered a new ammunition type into the database.")
+        else:
+            messagebox.showerror(title="Error", message="An error occurred: " + result)
+        popup.destroy()
+
+    button_cancel = ttk.Button(popup, text="Cancel", command=popup.destroy).grid(row=3, column=1, padx=10, pady=15)
+
+    popup.mainloop()
+
+
+# def popup_edit_card():
 
 
 class ShootingClub(tk.Tk):
@@ -141,8 +279,8 @@ class ShootingClub(tk.Tk):
 
         tk.Tk.__init__(self, *args, **kwargs)
 
-        tk.Tk.iconbitmap(self, default="logo_16_16.ico")
-        tk.Tk.wm_title(self, "Shooting Club")
+        # tk.Tk.iconbitmap(self, default="logo_16_16.ico")
+        tk.Tk.wm_title(self, "Schietvereniging - Prinses Juliana")
 
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
@@ -152,9 +290,11 @@ class ShootingClub(tk.Tk):
         menu_bar = tk.Menu(container)
         file_menu = tk.Menu(menu_bar, tearoff=0)
 
-        file_menu.add_command(label="New User", command=lambda: popup_new_user())
+        file_menu.add_command(label="Nieuw Lid", command=lambda: popup_new_user())
 
-        file_menu.add_command(label="New Weapon", command=lambda: popup_new_weapon())
+        file_menu.add_command(label="Nieuw Wapen", command=lambda: popup_new_weapon())
+
+        file_menu.add_command(label="Nieuwe Munitie", command=lambda: popup_new_ammunition())
 
         file_menu.add_separator()
 
@@ -166,7 +306,7 @@ class ShootingClub(tk.Tk):
 
         self.frames = {}
 
-        for F in (MainMenu, ScorePage, PageTwo):
+        for F in (MainMenu, ScorePage, FinancePage):
 
             frame = F(container, self)
 
@@ -193,7 +333,7 @@ class MainMenu(tk.Frame):
         button = ttk.Button(self, text="Submit a Score", command=lambda: controller.show_frame(ScorePage))
         button.pack()
 
-        button2 = ttk.Button(self, text="Visit Page 2", command=lambda: controller.show_frame(PageTwo))
+        button2 = ttk.Button(self, text="Finance Page", command=lambda: controller.show_frame(FinancePage))
         button2.pack()
 
 
@@ -207,15 +347,15 @@ class ScorePage(tk.Frame):
         button1 = ttk.Button(self, text="Main Menu", command=lambda: controller.show_frame(MainMenu))
         button1.pack()
 
-        button2 = ttk.Button(self, text="Visit Page 2", command=lambda: controller.show_frame(PageTwo))
+        button2 = ttk.Button(self, text="Finance Page", command=lambda: controller.show_frame(FinancePage))
         button2.pack()
 
 
-class PageTwo(tk.Frame):
+class FinancePage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Page Two", font=LARGE_FONT)
+        label = ttk.Label(self, text="Finance Page", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
         button1 = ttk.Button(self, text="Main Menu", command=lambda: controller.show_frame(MainMenu))
@@ -226,5 +366,5 @@ class PageTwo(tk.Frame):
 
 
 app = ShootingClub()
-app.geometry("1280x720")
+app.geometry("1080x720")
 app.mainloop()
